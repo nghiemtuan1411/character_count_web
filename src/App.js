d68bff635159fee57464d5dbff7e504a918029a4
Tuan <nghiemtuan1411@gmail.com>
2023-04-12 18:36:18 +0700

    add project

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  // the text entered by the user
+  const [text, setText] = useState('');
+
+  // word count
+  const [wordCount, setWordCount] = useState(0);
+
+  // character count
+  const [charCount, setCharCount] = useState(0);
+
+  const changeHandler = (event) => {
+    setText(event.target.value);
+  };
+
+  useEffect(() => {
+    // array of words
+    const words = text.split(' ');
+
+    // update word count
+    let wordCount = 0;
+    words.forEach((word) => {
+      if (word.trim() !== '') {
+        wordCount++;
+      }
+    });
+    setWordCount(wordCount);
+
+    // update char count (including whitespaces)
+    setCharCount(text.length);
+  }, [text]);
+
+  return (
+    <div className='container'>
+      <h2>TuanMath.com</h2>
+      <div>
+        <textarea
+          value={text}
+          onChange={changeHandler}
+          placeholder='What is on your mind?'
+        ></textarea>
+
+        <div>
+          <p className='word-count'>Word Count: {wordCount}</p>
+          <p className='char-count'>
+            Character Count (Including Whitespaces): {charCount}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
